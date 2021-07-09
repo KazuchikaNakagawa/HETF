9fabafb3ada5792c7db20ca9b283dc0e17017783
KazuchikaNakagawa <KazuchikaNakagawa@gmail.com>
2021-07-09 10:39:41 +0900

    Checked CMake settings

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main(int argc, char const *argv[]) {
   /* code */
-  vector<string> args(argc, argc + argv);
+  vector<string> args(argv, argc + argv);
   if (args.size() == 1) {
     /* help */
     cout << "HETF(High Efficient Text Formatter)" << endl;
